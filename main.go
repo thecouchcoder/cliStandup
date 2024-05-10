06cda1c82a7e9d06f24e75000ff434519407604b
Ashley Stallings <ashleystal@zillowgroup.com>
2024-05-10 10:40:53 -0500

    Implement delete and refactor for organization

--- main.go
+++ main.go
@@ -4,45 +4,8 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
 )
-
-type UpdateItem struct {
-	title       string
-	description string
-}
-
-func (u UpdateItem) Title() string {
-	return u.title
-}
-
-func (u UpdateItem) Description() string {
-	return u.description
-}
-
-func (u UpdateItem) FilterValue() string {
-	return u.description
-}
-
-type Model struct {
-	updates list.Model
-}
-
-func NewModel() Model {
-	sampleUpdate := UpdateItem{title: "Update", description: "This is a sample update"}
-
-	m := Model{
-		updates: list.New(
-			[]list.Item{sampleUpdate, sampleUpdate, sampleUpdate},
-			list.NewDefaultDelegate(),
-			0,
-			0),
-	}
-
-	m.updates.Title = "Sprint Updates"
-	return m
-}
 
 func (m Model) Init() tea.Cmd {
 	return nil
@@ -56,6 +19,8 @@ func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		switch msg.String() {
 		case "ctrl+c", "esc":
 			return m, tea.Quit
+		case "d":
+			m.updates.RemoveItem(m.updates.Index())
 		}
 	}
 
@@ -76,7 +41,7 @@ func main() {
 		os.Exit(1)
 	}
 	defer f.Close()
-	p := tea.NewProgram(NewModel())
+	p := tea.NewProgram(NewModel(), tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("Alas, there's been an error: %v", err)
 		os.Exit(1)
