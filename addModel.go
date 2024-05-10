e8b5448eadb17b7e9d9cf75c016885d4b310bc61
Ashley Stallings <ashleystal@zillowgroup.com>
2024-05-10 14:15:29 -0500

    Finish up add model help bar

--- addModel.go
+++ addModel.go
@@ -1,22 +1,63 @@
 package main
 
 import (
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
 	"github.com/charmbracelet/bubbles/textarea"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
+
+type KeyMap struct {
+	EscWriteMode key.Binding
+	EscViewMode  key.Binding
+	Write        key.Binding
+	Save         key.Binding
+}
+
+func DefaultKeyMap() KeyMap {
+	return KeyMap{
+		EscWriteMode: key.NewBinding(key.WithKeys("esc"), key.WithHelp("esc", "view")),
+		EscViewMode:  key.NewBinding(key.WithKeys("esc"), key.WithHelp("esc", "back")),
+		Write: key.NewBinding(
+			key.WithKeys("w"),
+			key.WithHelp("w", "write mode"),
+		),
+		Save: key.NewBinding(
+			key.WithKeys("enter"),
+			key.WithHelp("enter", "save"),
+		),
+	}
+}
 
 type AddModel struct {
 	textArea textarea.Model
+	help     help.Model
+	KeyMap   KeyMap
 }
+
+func (m AddModel) ShortHelp() []key.Binding {
+	if m.textArea.Focused() {
+		return []key.Binding{m.KeyMap.EscViewMode}
+	}
+	return []key.Binding{m.KeyMap.EscViewMode, m.KeyMap.Write, m.KeyMap.Save}
+}
+
+// Noop to satisfy the interface
+func (k AddModel) FullHelp() [][]key.Binding { return nil }
 
 func NewAddModel(width, height int) AddModel {
 	m := AddModel{
 		textArea: textarea.New(),
+		help:     help.New(),
+		KeyMap:   DefaultKeyMap(),
 	}
 	m.textArea.Placeholder = "Enter your update here"
 	m.textArea.Focus()
 	m.textArea.SetWidth(width)
-	m.textArea.SetHeight(height)
+	m.textArea.SetHeight(height - 1)
+	m.help.Width = width
+	m.help.ShowAll = false
 
 	return m
 }
@@ -57,7 +98,13 @@ func (m AddModel) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, cmd
 }
 
-func (m AddModel) View() string { return m.textArea.View() }
+func (m AddModel) View() string {
+	helpView := m.help.View(m)
+	textAreaView := m.textArea.View()
+
+	// TODO we could dynamically determine height incase of window resize
+	return lipgloss.JoinVertical(lipgloss.Left, textAreaView, helpView)
+}
 
 func (m AddModel) SaveUpdateCmd() tea.Msg {
 	return UpdateItem{description: m.textArea.Value()}
