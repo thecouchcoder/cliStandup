762cd73cada63a914f86cc21d4b2813dc26df3a8
Ashley Stallings <ashleystal@zillowgroup.com>
2024-05-11 07:41:31 -0500

    Use sqlc to generate db files

---
+++ db/query.sql
@@ -0,0 +1,8 @@
+-- name: GetUpdates :many
+SELECT * FROM updates ORDER BY created_at DESC;
+
+-- name: GetUpdatesSince :many
+SELECT * FROM updates WHERE created_at > ? ORDER BY created_at DESC;
+
+-- name: ArchiveUpdate :exec
+UPDATE updates SET archived = TRUE WHERE id = ?
